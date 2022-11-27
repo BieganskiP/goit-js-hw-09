097a60826d8570d219d1ea45d2e1b380184599ad
BieganskiP <bieganski1996@gmail.com>
2022-11-27 17:20:43 +0100

    update

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -23,7 +23,6 @@ const options = {
     } else {
       startBtn.removeAttribute('disabled');
       selectedDate = selectedDates[0];
-      todaysDate = new Date();
     }
   },
 };
@@ -33,20 +32,16 @@ const addLeadingZero = value => {
   return value.padStart(2, '0');
 };
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   todaysDate = new Date();
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   timerDays.innerHTML = days;
@@ -54,6 +49,7 @@ function convertMs(ms) {
   timerMinutes.innerHTML = addLeadingZero(String(minutes));
   timerSeconds.innerHTML = addLeadingZero(String(seconds));
 }
+
 startBtn.addEventListener('click', () => {
   startBtn.setAttribute('disabled', '');
   const timer = setInterval(() => {
